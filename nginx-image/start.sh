26a5df13af4064bfa388bc21659376187722879b
Alexander Morton <mortonprod@gmail.com>
2017-07-23 11:26:06 +0000

    WORKING WITH TEST MORTONPROD.CO.UK AND ALEXANDERMORTON.CO.UK

--- nginx-image/start.sh
+++ nginx-image/start.sh
@@ -5,11 +5,13 @@ echo build starting nginx config
 
 
 echo replacing ___my.example.com___/$MY_DOMAIN_NAME
-
+echo replacing ___my.example.com2___/$MY_DOMAIN_NAME2
 # Put your domain name into the nginx reverse proxy config.
 sed -i "s/___my.example.com___/$MY_DOMAIN_NAME/g" /etc/nginx/nginx.conf
-# Add your app's container IP and port into config
+sed -i "s/___my.example.com2___/$MY_DOMAIN_NAME2/g" /etc/nginx/nginx.conf
 
+
+echo nginx.conf: 
 cat /etc/nginx/nginx.conf
 echo .
 echo Firing up nginx in the background.
@@ -20,6 +22,13 @@ if [ -z "$MY_DOMAIN_NAME" ]; then
     echo "Need to set MY_DOMAIN_NAME (to a letsencrypt-registered name)."
     exit 1
 fi  
+
+if [ -z "$MY_DOMAIN_NAME2" ]; then
+    echo "Need to set MY_DOMAIN_NAME (to a letsencrypt-registered name)."
+    exit 1
+fi  
+
+
 
 # This bit waits until the letsencrypt container has done its thing.
 # We see the changes here bceause there's a docker volume mapped.
@@ -41,10 +50,42 @@ do
     sleep 2
 done
 
+
+
+
+
+echo Waiting for folder /etc/letsencrypt/live/$MY_DOMAIN_NAME2 to exist
+while [ ! -d /etc/letsencrypt/live/$MY_DOMAIN_NAME2 ] ;
+do
+    sleep 2
+done
+
+while [ ! -f /etc/letsencrypt/live/$MY_DOMAIN_NAME2/fullchain.pem ] ;
+do
+	echo Waiting for file fullchain.pem to exist
+    sleep 2
+done
+
+while [ ! -f /etc/letsencrypt/live/$MY_DOMAIN_NAME2/privkey.pem ] ;
+do
+	echo Waiting for file privkey.pem to exist
+    sleep 2
+done
+
+echo FOUND ALL FILES NEEDED.
+
+
+
 echo replacing ___my.example.com___/$MY_DOMAIN_NAME
+
+echo replacing ___my.example.com2___/$MY_DOMAIN_NAME2
+# Put your domain name into the nginx reverse proxy config.
 
 # Put your domain name into the nginx reverse proxy config.
 sed -i "s/___my.example.com___/$MY_DOMAIN_NAME/g" /etc/nginx/nginx-secure.conf
+sed -i "s/___my.example.com2___/$MY_DOMAIN_NAME2/g" /etc/nginx/nginx-secure.conf
+echo Secure nginx.conf: 
+cat /etc/nginx/nginx.conf
 
 # Add LE container IP and port into config
 #go!
